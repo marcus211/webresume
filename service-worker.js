c50e4d9028e9ed08eb31a56cc61b96a15507f667
marcus211 <saiboy211@gmail.com>
2019-11-12 15:30:33 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/webresume/precache-manifest.6d7488fd65bd8a7f5de3aeb95ade3721.js"
+  "/webresume/precache-manifest.5da82b5b0e56be6bde29e14904dab31d.js"
 );
 
 workbox.clientsClaim();
